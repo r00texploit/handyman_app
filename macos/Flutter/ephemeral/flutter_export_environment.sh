60081da48e48c273785172ba1390c9fbb7612468
r00texploit <halimghazi7@gmail.com>
2024-07-06 12:10:03 +0400

    init

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/bin/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/murad/Desktop/New Folder/codecanyon-11.5.3-handyman-service-flutter-ondemand-home-services-app-with-complete-solution (1)/handyman_mainfile_sourcecode/Mobile app/user app/latest/handyman_user_flutter_v11.5.3"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=11.5.3"
+export "FLUTTER_BUILD_NUMBER=11.5.3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
